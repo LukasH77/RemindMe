9687310078f582a9700c4c3b3eff32be3d918b0d
Lukas <lukas.hofmann77@gmail.com>
2020-11-16 19:46:20 +0100

    Created the new project and repository. Put in Navigation with two fragments. Created an early layout. Added a button that sends a notification when clicked.

--- app/src/main/java/com/example/fancytimes/home/HomeFragment.kt
+++ app/src/main/java/com/example/fancytimes/home/HomeFragment.kt
@@ -1,11 +1,15 @@
 package com.example.fancytimes.home
 
+import android.app.*
+import android.content.Context
+import android.os.Build
 import android.os.Bundle
 import android.view.*
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationManagerCompat
 import androidx.fragment.app.Fragment
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
-import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.example.fancytimes.R
 import com.example.fancytimes.databinding.FragmentHomeBinding
@@ -14,6 +18,7 @@ class HomeFragment : Fragment() {
 
     private lateinit var binding: FragmentHomeBinding
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -21,7 +26,40 @@ class HomeFragment : Fragment() {
         binding = DataBindingUtil.inflate(layoutInflater, R.layout.fragment_home, container, false)
         setHasOptionsMenu(true)
 
+
+
+
+
+        val instantNotificationButton = binding.bInstantNotification
+
+        createNotificationChannel()
+
+        val notification =
+            Notification.Builder(requireActivity(), "FancyTimes notifications")
+                .setSmallIcon(R.drawable.access_time_24px)
+                .setContentTitle("Fancy Time!!").setContentText("It's time, it is fancy o'clock!")
+
+        instantNotificationButton.setOnClickListener {
+            with(NotificationManagerCompat.from(requireActivity())) {
+                notify(1, notification.build())
+            }
+        }
+
         return binding.root
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "Notification Channel"
+            val text = "Fancy Time notifications"
+            val importance = NotificationManager.IMPORTANCE_HIGH
+            val channel = NotificationChannel("FancyTimes notifications", name, importance).apply {
+                description = text
+            }
+            val notificationManager: NotificationManager =
+                activity?.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
