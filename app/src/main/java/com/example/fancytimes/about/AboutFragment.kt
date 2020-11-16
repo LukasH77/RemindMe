02688a0623f39d1eb7fa7d2599ec5bb5f06cf379
Lukas <lukas.hofmann77@gmail.com>
2020-11-16 17:26:26 +0100

    Initial repository

---
+++ app/src/main/java/com/example/fancytimes/about/AboutFragment.kt
@@ -0,0 +1,26 @@
+package com.example.fancytimes.about
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.fancytimes.R
+import com.example.fancytimes.databinding.FragmentAboutBinding
+
+class AboutFragment : Fragment() {
+
+    private lateinit var binding: FragmentAboutBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(layoutInflater, R.layout.fragment_about, container, false)
+
+        return binding.root
+    }
+
+
+}
---
+++ app/src/main/java/com/example/fancytimes/home/HomeFragment.kt
@@ -0,0 +1,38 @@
+package com.example.fancytimes.home
+
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.example.fancytimes.R
+import com.example.fancytimes.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(layoutInflater, R.layout.fragment_home, container, false)
+        setHasOptionsMenu(true)
+
+        return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.overflow_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(
+            item,
+            requireView().findNavController()
+        ) || super.onOptionsItemSelected(item)
+    }
+}
